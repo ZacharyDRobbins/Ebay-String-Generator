ab612528a2e2aec62db9e008925e6518826b48a8
Zachary Robbins <37849198+ZacharyDRobbins@users.noreply.github.com>
2019-02-06 14:49:24 -0700

    Add files via upload

---
+++ Ebay String Generator/Ebay String Generator/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace Ebay_String_Generator
+{
+    partial class Title_Output
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Set = new System.Windows.Forms.TextBox();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.Number = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Rarity = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Amount = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Title_Output2 = new System.Windows.Forms.TextBox();
+            this.Description_Output = new System.Windows.Forms.TextBox();
+            this.Rarity.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Set
+            // 
+            this.Set.Location = new System.Drawing.Point(91, 102);
+            this.Set.Name = "Set";
+            this.Set.Size = new System.Drawing.Size(482, 38);
+            this.Set.TabIndex = 0;
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(91, 225);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(482, 38);
+            this.Name.TabIndex = 1;
+            // 
+            // Number
+            // 
+            this.Number.Location = new System.Drawing.Point(91, 342);
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(482, 38);
+            this.Number.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Set";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter Card Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(258, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter Card Number";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 791);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(494, 154);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "MAKE IT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(722, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(316, 61);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Title Output:";
+            // 
+            // Rarity
+            // 
+            this.Rarity.Controls.Add(this.radioButton5);
+            this.Rarity.Controls.Add(this.radioButton4);
+            this.Rarity.Controls.Add(this.radioButton3);
+            this.Rarity.Controls.Add(this.radioButton2);
+            this.Rarity.Controls.Add(this.radioButton1);
+            this.Rarity.Location = new System.Drawing.Point(91, 398);
+            this.Rarity.Name = "Rarity";
+            this.Rarity.Size = new System.Drawing.Size(219, 298);
+            this.Rarity.TabIndex = 11;
+            this.Rarity.TabStop = false;
+            this.Rarity.Text = "Rarity";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(18, 256);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(202, 36);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Secret Rare";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(18, 202);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(180, 36);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Ultra Rare";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(18, 149);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(113, 36);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Rare";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(18, 94);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(196, 36);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Uncommon";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 37);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(166, 36);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Common";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton7);
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Location = new System.Drawing.Point(330, 398);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 134);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Foil";
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(28, 79);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(111, 36);
+            this.radioButton7.TabIndex = 1;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Holo";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(28, 37);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(157, 36);
+            this.radioButton6.TabIndex = 0;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Reverse";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 37);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(94, 36);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "GX";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Location = new System.Drawing.Point(330, 547);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(243, 149);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Extra";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 79);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(143, 36);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "Full Art";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(722, 359);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(481, 61);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Description Output:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(103, 708);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 32);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Amount";
+            // 
+            // Amount
+            // 
+            this.Amount.Location = new System.Drawing.Point(222, 708);
+            this.Amount.Name = "Amount";
+            this.Amount.Size = new System.Drawing.Size(100, 38);
+            this.Amount.TabIndex = 19;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(677, 791);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(557, 154);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Title_Output2
+            // 
+            this.Title_Output2.Location = new System.Drawing.Point(727, 118);
+            this.Title_Output2.Multiline = true;
+            this.Title_Output2.Name = "Title_Output2";
+            this.Title_Output2.Size = new System.Drawing.Size(854, 201);
+            this.Title_Output2.TabIndex = 21;
+            // 
+            // Description_Output
+            // 
+            this.Description_Output.Location = new System.Drawing.Point(727, 465);
+            this.Description_Output.Multiline = true;
+            this.Description_Output.Name = "Description_Output";
+            this.Description_Output.Size = new System.Drawing.Size(854, 281);
+            this.Description_Output.TabIndex = 22;
+            // 
+            // Title_Output
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1669, 957);
+            this.Controls.Add(this.Description_Output);
+            this.Controls.Add(this.Title_Output2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Amount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Rarity);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Number);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.Set);
+        //    this.Name = "Title_Output";
+            this.Text = "Form1";
+            this.Rarity.ResumeLayout(false);
+            this.Rarity.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Set;
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.TextBox Number;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox Rarity;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Amount;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Title_Output2;
+        private System.Windows.Forms.TextBox Description_Output;
+    }
+}
+
---
+++ Ebay String Generator/Ebay String Generator/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ebay_String_Generator
+{
+    public partial class Title_Output : Form
+    {
+        public Title_Output()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String part1 = "";
+            String name = Name.Text;
+            if (checkBox1.Checked == true)
+                name = name + " GX";
+            if (checkBox2.Checked == true)
+                name = name + " FA";
+            part1 = "Pokemon " + Set.Text + "-" + name + "-" + Number.Text + "-";
+
+
+            String rarity = radioButton1.Text;
+            if (radioButton2.Checked == true)
+            { rarity = radioButton2.Text; }
+            if (radioButton3.Checked == true)
+            { rarity = radioButton3.Text; }
+            if (radioButton4.Checked == true)
+            { rarity = radioButton4.Text; }
+            if (radioButton5.Checked == true)
+            { rarity = radioButton5.Text; }
+
+            String foil = "";
+            if (radioButton6.Checked == true)
+            { foil = radioButton6.Text; }
+            if (radioButton7.Checked == true)
+            { foil = radioButton7.Text; }
+
+            String end = "";
+            if (Amount.Text == "1" || Amount.Text == "")
+            { end = "NM"; }
+            else
+            { end = "NM " + Amount.Text + " Cards"; }
+            Title_Output2.Text = part1 + rarity +" " + foil + " " + end;
+
+            Description_Output.Text = Amount.Text + " " + rarity + " " + foil + " "+ name + ", " + Number.Text + ", " + Set.Text + "\n\nNever Been Played, Card Went Straight from the Pack into a Sleeve\nNear Mint Cards, Double Sleeved with Penny Sleeves and Hard Sleeve";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Set.Text = "";
+            Name.Text = "";
+            Number.Text = "";
+            Amount.Text = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+            radioButton5.Checked = false;
+            radioButton6.Checked = false;
+            radioButton7.Checked = false;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+        }
+    }
+}
